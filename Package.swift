d3c09735af855d808c01d1fa5ce899da4c6e1630
Spela Ziherl <spela.ziherl@kamino.si>
2024-10-30 08:55:55 +0100

    add zip

---
+++ Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/https://github.com/Paddington12345/webRTC-SDK/WebRTC.xcframework.zip",
+            checksum: "3c77fe4ff47aaa7fd55cb0c9dd6b5efbaeeb87ecfb2f13a26869a644b1024b48"
+        ),
+    ]
+)
